4d19c2b77080ce1b0751bc08aee4aaf09383dab4
Jem Acuna <dev.jemuelacuna@gmail.com>
2024-12-18 15:58:11 +0800

    Additional

--- js/hacking.js
+++ js/hacking.js
@@ -1,4 +1,4 @@
 console.log('%c Looking for something, you Gnarlings??? ', 'background: #222; color: #bada55');
 console.log('%c Worry yourself no more. I am Groot! ', 'background: #222; color: #bada55');
 
-let i = 1; while(i <= 10) { console.log(`${i} for you...`); i++; }
+let i = 1; while(i <= 10) { console.log(`${i} 🌷 for you...`); i++; }
